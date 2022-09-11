816a77b400a4031937c9bed8a9016f0e5e7eee34
priscapaul <priscapaul19@gmail.com>
2022-09-11 16:10:42 -0700

    puts function

--- 0x00-hello-world/4-puts.c
+++ 0x00-hello-world/4-puts.c
@@ -1,17 +1,31 @@
-#!/bin/bash
 #include <stdio.h>
 
+
+
 /**
- *Description: main - prints out a string to stdout.
- *Return: 0 if success.
- */
 
- int main(void)
+ * Description: main - Prints out a string to stdout.
 
- {
- 
-       puts("\"programming is like building a multilingual puzzle");
-       
-       return(0);
+ * Return: 0 if success.
 
- }
+*/
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+puts("\"Programming is like building a multilingual puzzle");
+
+
+
+return (0);
+
+
+
+}
