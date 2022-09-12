c94afb8160f58b1575b7314302d2fe4ebc0ac2a5
priscapaul <priscapaul19@gmail.com>
2022-09-12 08:58:12 -0700

    fixed :documentation style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
- /**
-  * Description: main - Prints out a string to stdout.
-  * Return: 0 if success.
- */
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+*/
 
- int main(void)
+int main(void)
+{
+  
+        puts("\"Programming is like building a multilingual puzzle,\n");
 
- {
+        return (0);
 
-          puts("\"programming is like building a multilingual puzzle");
-
-          return (0);
-
- }
+}
