a23f230fd57d902aff0f38a8d85949b06d7d916e
priscapaul <priscapaul19@gmail.com>
2022-09-11 17:48:55 -0700

    fix betty syntax

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
 
- * Description: main - Prints out a string to stdout.
+ * main - main block
 
- * Return: 0 if success.
+ * Return: 0
 
-*/
-
-
+ */
 
 int main(void)
 
-
-
 {
-
-
 
 puts("\"Programming is like building a multilingual puzzle");
 
-
-
 return (0);
-
-
 
 }
