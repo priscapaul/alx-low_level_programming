a9e9a100853fe5462d2c670df459dc286537e58c
priscapaul <priscapaul19@gmail.com>
2022-09-12 08:36:00 -0700

    fix: using puts function in C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 
+ /**
+  * Description: main - Prints out a string to stdout.
+  * Return: 0 if success.
+ */
 
+ int main(void)
 
-/**
+ {
 
- * Description: main - Prints out a string to stdout.
+          puts("\"programming is like building a multilingual puzzle");
 
- * Return: 0 if success.
+          return (0);
 
-*/
-
-
-
-int main(void)
-
-
-
-{
-
-
-
-puts("\"Programming is like building a multilingual puzzle");
-
-
-
-return (0);
-
-
-
-}
+ }
