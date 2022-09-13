8e07b9df55493997b284342c7b33bf416dfb91f3
priscapaul <priscapaul19@gmail.com>
2022-09-12 19:07:11 -0700

    generates assembly codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
+
  * Description: main - Prints out a string to stdout.
+
  * Return: 0 if success.
+
 */
 
 int main(void)
+
 {
-  
+
         puts("\"Programming is like building a multilingual puzzle,\n");
 
         return (0);
