462178793c6556c28fb5c1633ad991c2d0eab1aa
priscapaul <priscapaul19@gmail.com>
2022-09-12 20:14:26 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - describe output
+ *
+ * Return: 0 if success.
+*/
+
+int main(void)
+
+{
+char i;
+int j;
+long int k;
+long long int 1;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+return (0);
+}
