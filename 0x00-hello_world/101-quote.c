0f36622c57d6f6e5397493d9cf7ebf34c475d005
priscapaul <priscapaul19@gmail.com>
2022-09-12 19:35:34 -0700

    fix:betty code style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 /**
- *  * main - shows a message.
+ *  * main -shows a message.
  *   * Return: 1 if success.
 */
 
@@ -12,8 +12,8 @@ int main(void)
 
 {
 
-         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-         return (1);
+ return (1);
 
 }
