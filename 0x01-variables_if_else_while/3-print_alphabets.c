606ea3dc06c936764739b395687c4dda66a577af
priscapaul <priscapaul19@gmail.com>
2022-09-18 15:24:22 -0700

    lower and upper case alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,3 +2,30 @@
 
 /**
  * main - Prints lower and upper case alphabets to stdout.
+ * Return: 0 if successful.
+*/
+
+int main(void)
+{
+char lower;
+char upper;
+
+lower = 'a';
+upper = 'A';
+
+while (lower <= 'z')
+{
+putchar(lower);
+lower++;
+}
+
+while (upper <= 'Z')
+{
+putchar(upper);
+upper++;
+}
+
+putchar('\n');
+
+return (0);
+}
